b1532030a8081ade146ac6c00f3728b96b92087b
Joseph Barbone <JoeBarbone@aol.com>
2022-07-04 16:28:22 -0400

    12.3.5 added join

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,7 @@
-CREATE TABLE candidates (
-  
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 
-);
+
 
 CREATE TABLE parties (
 
@@ -14,3 +10,16 @@ CREATE TABLE parties (
   description text
 
 );
+
+
+CREATE TABLE candidates (
+  
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+
+);
+
